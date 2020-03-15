620b38b7bb00d613fd67a09f2cc9d0ada0fe7754
brandonmongo <brandonmongo@hotmail.com>
2020-03-15 06:20:01 +0000

    add corrections for drinks script

--- script/what-can-i-drink.js
+++ script/what-can-i-drink.js
@@ -6,15 +6,15 @@ function whatCanIDrink(age) {
         return "Drink Toddy"
 
     }
-    else if (age < 18) {
+    else if (age < 18 && age >=14) {
         return "Drink Coke"
 
     }
-    else if (age < 21) {
+    else if (age < 21 && age >=18) {
         return "Drink Beer"
 
     }
-    else if (age < 130) {
+    else if (age < 130 && age >=21) {
         return "Drink Whisky"
 
     }
